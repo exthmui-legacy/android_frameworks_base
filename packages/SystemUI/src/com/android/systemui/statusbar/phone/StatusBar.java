9e16c5c408871d458c192b89af98bb3cf30c884c
Pranav Vashi <neobuddy89@gmail.com>
2020-01-16 00:37:03 +0530

    Bring back ticker for music ticker and Android 10 fixes
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -484,8 +484,8 @@ public class StatusBar extends SystemUI implements DemoMode,
     private boolean mJustPeeked;
 
     // status bar notification ticker
-    private int mTickerEnabled;
-    private Ticker mTicker;
+    public int mTickerEnabled;
+    public Ticker mTicker;
     private boolean mTicking;
     private int mTickerAnimationMode;
     private int mTickerTickDuration;
@@ -518,6 +518,12 @@ public class StatusBar extends SystemUI implements DemoMode,
             mLinger = BRIGHTNESS_CONTROL_LINGER_THRESHOLD + 1;
         }
     };
+
+    public void resetTrackInfo() {
+        if (mTicker != null) {
+            mTicker.resetShownMediaMetadata();
+        }
+    }
 
     // ensure quick settings is disabled until the current user makes it through the setup wizard
     @VisibleForTesting
@@ -1167,6 +1173,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                 mHeadsUpManager, mStatusBarWindow, mStackScroller, mDozeScrimController,
                 mScrimController, mActivityLaunchAnimator, mDynamicPrivacyController,
                 mNotificationAlertingManager, rowBinder);
+        mPresenter.addCallback(this);
 
         mNotificationListController =
                 new NotificationListController(
@@ -1225,6 +1232,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         mGutsManager = Dependency.get(NotificationGutsManager.class);
         mLockscreenUserManager = Dependency.get(NotificationLockscreenUserManager.class);
         mMediaManager = Dependency.get(NotificationMediaManager.class);
+        mMediaManager.addCallback(this);
         mNotificationInterruptionStateProvider =
                 Dependency.get(NotificationInterruptionStateProvider.class);
         mNotificationListener = Dependency.get(NotificationListener.class);
